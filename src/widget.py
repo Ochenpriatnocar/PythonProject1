34e34135cfd0537d688654a274ba5d3b457d5bd1
Ildar khusnutdinov <ihia@yandex.ru>
2025-06-23 21:35:03 +0500

    add func mask accaunt or card numb

---
+++ src/widget.py
@@ -0,0 +1,18 @@
+from src.masks import get_mask_account
+from src.masks import get_mask_card_number
+
+
+def mask_account_card(account_info: str) -> str:
+    """Функция маскировки номера карты или счета"""
+
+    accont_or_paiment_sistem, account_or_card_number = account_info.rsplit(" ", maxsplit=1)
+
+    if (
+        accont_or_paiment_sistem == "Счет"
+        or accont_or_paiment_sistem == "счет"
+        or accont_or_paiment_sistem == "Счёт"
+        or accont_or_paiment_sistem == "счёт"
+    ):
+        return accont_or_paiment_sistem + " " + get_mask_account(account_or_card_number)
+    else:
+        return accont_or_paiment_sistem + " " + get_mask_card_number(account_or_card_number)
