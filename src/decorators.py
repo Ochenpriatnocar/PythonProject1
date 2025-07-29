9dc1deef5cf7884a6a6e8ffa965d0cd6a8328af2
Ildar khusnutdinov <ihia@yandex.ru>
2025-07-30 00:08:47 +0500

    добавил модуль test_decorators.py, добавил описание в readmy.txt

--- src/decorators.py
+++ src/decorators.py
@@ -12,22 +12,29 @@ def log(filename=None):
                 time_2 = time.time()
                 name_func = func.__name__
                 if filename:
-                    file = open(filename, "a", encoding="utf-8")
-                    file.write(f"Начало: {time_1} \nФункция {name_func} ок. Результат: {result} \nКонец: {time_2}\n\n")
+                    file = open(filename, "w", encoding="utf-8")
+                    file.write(f"Начало: {time_1} \nФункция {name_func} ок. Результат: {result}\nКонец: {time_2}\n\n")
                     file.close()
                 else:
-                    print(f"Начало: {time_1} \nФункция {name_func} ок. Результат: {result} \nКонец: {time_2}")
-            except Exception as e:
-                print(f"Начало: {time_1}\n{func.__name__} error: {e}. Inputs: {args}, {kwargs}\n{time_2}")
+                    print(f"Начало: {time_1} \nФункция {name_func} ок. Результат: {result}\nКонец: {time_2}")
+            except TypeError:
+                name_func = func.__name__
+                if filename:
+                    file = open(filename, "w", encoding="utf-8")
+                    file.write(f"{name_func} error: TypeError. Inputs: {args}, {kwargs}")
+                    file.close()
+                else:
+                    print(f"{name_func} error: TypeError. Inputs: {args}, {kwargs}")
 
         return wrapper
 
     return decorator
 
 
-# @log()
+# @log(filename="2.txt")
 # def summa(a, b):
+#     """Сумма двух чисел"""
 #     return a + b
 #
 #
-# summa(2, 3)
+# summa("2", 3)
---
+++ tests/test_decorators.py
@@ -0,0 +1,51 @@
+import pytest
+
+from src.decorators import log
+
+
+def test_log_file():
+    """Проверка корректности ведения записи в лог файл"""
+
+    @log(filename="1.txt")
+    def summa(x, y):
+        return x + y
+
+    summa(1, 2)
+    with open("1.txt", "r", encoding="utf-8") as file:
+        assert file.readlines()[1] == "Функция summa ок. Результат: 3\n"
+
+
+def test_log(capsys):
+    """Проверка корректности записи в консоли"""
+
+    @log()
+    def summa(x, y):
+        return x + y
+
+    summa(1, 2)
+    captured = capsys.readouterr()
+    assert captured.out[captured.out.find("Ф") : captured.out.find("К")] == "Функция summa ок. Результат: 3\n"
+
+
+def test_error_log_file():
+    """Проверка корректности ведения записи об ошибке в лог файл"""
+
+    @log(filename="1.txt")
+    def summa(x, y):
+        return x + y
+
+    summa("1", 2)
+    with open("1.txt", "r", encoding="utf-8") as file:
+        assert file.readlines()[0] == "summa error: TypeError. Inputs: ('1', 2), {}"
+
+
+def test_error_log(capsys):
+    """Проверка корректности записи об ошибке в консоли"""
+
+    @log()
+    def summa(x, y):
+        return x + y
+
+    summa("1", 2)
+    captured = capsys.readouterr()
+    assert captured.out[:-1] == "summa error: TypeError. Inputs: ('1', 2), {}"
