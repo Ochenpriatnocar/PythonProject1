6c3dcbf2e0df198e98736001ae316b3aaf40891f
Ildar khusnutdinov <ihia@yandex.ru>
2025-08-10 13:28:52 +0500

    Моck для utils

--- src/utils.py
+++ src/utils.py
@@ -9,14 +9,3 @@ def open_file(link_to_file) -> list:
         return data
     except:
         return []
-
-
-# data_str = open_file('../data/operations.json')
-# print(data_str)
-# print(type(data_str))
-#
-# for x in data_str:
-#     if x == {}:
-#         continue
-#     else:
-#         print(x["operationAmount"]["amount"])
